ec1989d1c5641c4484ab0ba9cfc34c474879c4a1
samishoukat12 <samishoukat12@gmail.com>
2025-01-27 14:23:02 +0500

    add google cast to react native

--- android/app/src/main/java/com/googlecast/MainActivity.kt
+++ android/app/src/main/java/com/googlecast/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.googlecast
 
+import com.google.android.gms.cast.framework.CastContext
+
+import android.os.Build
+import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // Set the theme to AppTheme BEFORE onCreate to support
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    setTheme(R.style.AppTheme);
+    super.onCreate(null)
+// @generated begin react-native-google-cast-onCreate - expo prebuild (DO NOT MODIFY) sync-5e616d57ec06f5b5c30139e226e4412bb505ab6f
+    CastContext.getSharedInstance(this)
+// @generated end react-native-google-cast-onCreate
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
